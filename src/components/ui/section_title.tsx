6cc2ce432d48174d3889900477128c16fb7be2c2
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-05-15 01:15:27 +0300

    add : style for section title

--- src/components/ui/section_title.tsx
+++ src/components/ui/section_title.tsx
@@ -6,9 +6,15 @@ interface Props{
 
 const SectionTitle = (props:Props) => {
     return (
-        <h3  className=" text-[#304336d9] text-2xl">
-            {props.label}
-        </h3>
+        <div className="flex flex-col space-y-2">
+            <h3  className=" text-[#304336d9] text-2xl">
+                {props.label}
+            </h3>
+            <div className="flex items-center">
+                <div className="bg-[#304336d9] rounded-full h-3 w-3"/>
+                <div className="w-36 h-1 bg-[#304336d9]" />
+            </div>
+        </div>
     );
 };
 
