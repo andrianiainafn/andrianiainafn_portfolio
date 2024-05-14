bab9da296c75a47ba199686f8472c4e66342b395
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-05-15 01:14:37 +0300

    fix : space [add padding]

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -3,7 +3,7 @@ import SectionTitle from "@/components/ui/section_title";
 
 const Contact = () => {
     return (
-        <div className="h-screen" id="contact">
+        <div className="h-screen px-[5%]" id="contact">
             <SectionTitle label="Contact"/>
         </div>
     );
