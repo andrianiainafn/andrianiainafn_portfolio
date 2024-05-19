3ebf3a54d82d21c7f06111b4198af0364d98d10c
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-05-19 19:04:04 +0300

    fix border error

--- src/components/ui/background-gradient-animation.tsx
+++ src/components/ui/background-gradient-animation.tsx
@@ -89,7 +89,7 @@ export const BackgroundGradientAnimation = ({
     return (
         <div
             className={cn(
-                "h-screen back w-screen relative overflow-hidden top-0 left-0 bg-[linear-gradient(40deg,var(--gradient-background-start),var(--gradient-background-end))]",
+                "h-screen back w-screen relative !m-0 overflow-hidden top-0 left-0 bg-[linear-gradient(40deg,var(--gradient-background-start),var(--gradient-background-end))]",
                 containerClassName
             )}
         >
