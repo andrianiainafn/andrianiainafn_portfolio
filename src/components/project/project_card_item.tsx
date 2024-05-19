7d8aba777ef674f780af1e502a0c83b592943d08
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-05-19 19:03:03 +0300

    add border animation

--- src/components/project/project_card_item.tsx
+++ src/components/project/project_card_item.tsx
@@ -5,6 +5,7 @@ import ProjectTech from "@/components/project/project_tech";
 import ProjectExternalLink from "@/components/project/project_external_link";
 import ProjectDescription from "@/components/project/project_description";
 import {number} from "prop-types";
+import {BorderBeam} from "@/components/ui/border_animate";
 
 interface Props{
     project:IProject
@@ -17,17 +18,19 @@ const ProjectCardItem = (props:Props) => {
         <>
             {
                 (number % 2) === 0 ? (
-                    <div className=" flex w-full relative justify-between text-white">
+                    <div className=" flex w-full  relative justify-between text-white">
                         <ProjectDescription project={project} even={true}/>
-                        <div className="w-[60%]">
-                            <Image src={`/${project.image}`} alt={project.title} width={800} height={600}/>
+                        <div className="relative w-[60%] overflow-hidden rounded-lg">
+                            <Image className="rounded-lg" src={`/${project.image}`} alt={project.title} width={800} height={600}/>
+                            <BorderBeam size={250} duration={12} delay={9} />
                         </div>
                     </div>
 
                 ):(
                     < div className=" flex w-full relative justify-between text-white">
-                        <div className="w-[60%]">
-                            <Image src={`/${project.image}`} alt={project.title} width={800} height={600}/>
+                        <div className="relative rounded-lg overflow-hidden w-[60%]">
+                            <Image className="rounded-lg" src={`/${project.image}`} alt={project.title} width={800} height={600}/>
+                            <BorderBeam size={250} duration={12} delay={9} />
                         </div>
                         <ProjectDescription project={project} even={false}/>
                     </div>
--- src/components/project/project_description.tsx
+++ src/components/project/project_description.tsx
@@ -11,15 +11,15 @@ interface Props{
 const ProjectDescription = (props:Props) => {
     const {project,even} = props
     return (
-        <div className={`flex  flex-col space-y-4 w-[40%] justify-start ${even ? '' : 'items-end'}`}>
+        <div className={`flex above flex-col space-y-4 w-[40%] justify-start ${even ? '' : 'items-end'}`}>
             <h5 className=" text-blue-500">
                 {project.type}
             </h5>
             <h3 className="text-2xl">
                 {project.title}
             </h3>
-            <div className={`absolute w-[50%] flex flex-col top-[20%] space-y-5  bg-opacity-25 ${even ? '' : 'items-end'}`}>
-                <div className="bg-[#304336d9] p-2">
+            <div className={`absolute w-[50%]  flex flex-col top-[20%] space-y-5  bg-opacity-25 ${even ? '' : 'items-end'}`}>
+                <div className="bg-[#304336d9] opacity-60 hover:opacity-100  transition-opacity  rounded-lg p-2">
                     <p className="opacity-50 ">
                         {project.description}
                     </p>
