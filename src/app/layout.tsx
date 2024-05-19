68acc074a70edf608d16e335a3002b0a39db2407
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-05-19 18:58:00 +0300

    feat: change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter,Montserrat  } from "next/font/google";
 import "./globals.css";
 
+const montserrat = Montserrat({ subsets: ["latin"] });
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>{children}</body>
     </html>
   );
 }
