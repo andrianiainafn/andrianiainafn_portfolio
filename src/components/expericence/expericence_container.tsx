eb1439d6e918a0891012b40683f98b917da71700
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-05-23 15:11:16 +0300

    fix tracing beam height

--- src/components/expericence/expericence_container.tsx
+++ src/components/expericence/expericence_container.tsx
@@ -94,22 +94,4 @@ const dummyContent = [
         image:
             "/gm-i.png",
     },
-    {
-        title: "Lorem Ipsum Dolor Sit Amet",
-        description: (
-            <>
-                <p>
-                    Ex irure dolore veniam ex velit non aute nisi labore ipsum occaecat
-                    deserunt cupidatat aute. Enim cillum dolor et nulla sunt exercitation
-                    non voluptate qui aliquip esse tempor. Ullamco ut sunt consectetur
-                    sint qui qui do do qui do. Labore laborum culpa magna reprehenderit ea
-                    velit id esse adipisicing deserunt amet dolore. Ipsum occaecat veniam
-                    commodo proident aliqua id ad deserunt dolor aliquip duis veniam sunt.
-                </p>
-            </>
-        ),
-        badge: "Launch Week",
-        image:
-            "/chaaat.png",
-    },
 ];
