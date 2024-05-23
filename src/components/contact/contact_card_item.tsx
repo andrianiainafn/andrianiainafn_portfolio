a5b6269a28b87ec3e59744e835ddd98df34e45d3
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-05-23 14:46:07 +0300

    add padding to card item

--- src/components/contact/contact_card_item.tsx
+++ src/components/contact/contact_card_item.tsx
@@ -11,7 +11,7 @@ const ContactCardItem = (props:Props) => {
         <div className="  flex flex-col  space-y-2 relative  p-2">
             <div className="w-[60px] h-[40px] bg-[#304336d9] rounded-full absolute z-0 top-[50%]
                  left-[50%] transform translate-x-[-50%] translate-y-[-50%] blur-xl"/>
-            <div className="above flex backdrop-filter flex-col space-y-3 backdrop-blur-[2px] blurText bg-white/5 rounded-lg  p-2">
+            <div className="above flex backdrop-filter flex-col space-y-3 backdrop-blur-[2px] blurText bg-white/5 rounded-lg  p-4">
                 {contact.icon}
                 <h5 className="cursor-pointer opacity-75">{contact.label}</h5>
                 <span className=" opacity-50 text-nowrap">{contact.value}</span>
